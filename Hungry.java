70ecd10f130f8f8205961f80bd5374bd7432fd2b
Siddharth Gupta <sidgupta998@gmail.com>
2021-02-17 12:24:32 +0530

    Hungry prompt added

--- Hungry.java
+++ Hungry.java
@@ -1,10 +1,20 @@
 package com.git;
 
+import java.util.Scanner;
+
 public class Hungry {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am hungry mate");
+		System.out.println("Are u hungry");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		if(str.equalsIgnoreCase("Yes"))
+			System.out.println("Eat Samosa");
+		else
+			System.out.println("Do your homework");
+
 
 	}
 
